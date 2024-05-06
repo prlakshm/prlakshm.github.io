fc45517e0b4826a5991a1505b0e31e1b1e3c4068
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-05-06 13:05:03 -0400

    Feat: case study heading done

--- src/pages/case-study-1/CaseStudy1.tsx
+++ src/pages/case-study-1/CaseStudy1.tsx
@@ -1,10 +1,13 @@
 import Footer from "../../components/Footer";
-import "./case-study.css";
+import "./case-study-1.css";
 
 function CaseStudy1() {
   return (
-    <div className="case-study1">
-        
+    <div className="case-study-1">
+        <div className="title-block">
+            <h1>Devlopment</h1>
+            <img src="/case-study-1/skew-carousel.png" alt="Examples of sandwitch item cards from web app"/>
+        </div>
 
         <Footer />
     </div>
