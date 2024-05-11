2fdbda7469bf435218960357f00643007071bfc1
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-05-10 22:52:16 -0400

    Fix: change to hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/home/Home";
 import Projects from "./pages/projects/Projects";
