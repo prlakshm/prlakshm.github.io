6a8841e9f245c6c0b3348cd79486dc746310cc44
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-05-10 22:18:46 -0400

    Fix: import react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/home/Home";
