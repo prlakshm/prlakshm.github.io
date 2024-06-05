b93239af035b8cefd77983e2346819eed05a8818
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-06-05 15:20:32 -0400

    Feat: attempting spline scene in home page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -10,7 +10,18 @@ function About() {
     
   return (
     <div className="about">
-      <div></div>
+      <div className="main">
+      <div className="about-me">
+        <h1>About Me</h1>
+        <p>I'm Pranavi 👋, a <span className="bold">sophomore at Brown University</span> studying computer science. I name all my ML models and paste pictures of websites I've built in my room! 
+            When I'm not coding, I'm singing, dancing, and having a good time with my friends.</p>
+            <p>I'm part of <span className="bold">Full Stack @ Brown</span>, where I work in a team of designers and developers to code websites for student clubs. I'm also a member of <span className="bold">Rewriting the Code</span>, a global organization that helps empower and connect women in CS. 
+                Previously, I was an encryption intern at <a href="https://www.mantech.com/" target="_blank">ManTech</a>, where I learned I love algorithms, and currently am a data science intern at <a href="https://www.carync.gov/" target="_blank">Town of Cary</a>, a smart city and the <a href="https://livability.com/best-places/2024-top-100-best-places-to-live-in-the-us/" target="_blank">#2 best city to live in the US</a>!
+            </p>
+            <p>Outside of CS, I take literary arts classes at Brown where I explore different forms of storytelling. <span className="bold">I love movies</span> (ask me for recs!) and recently got into screenwriting!  </p>
+            <p>If you're also interested in any of these things, I'd love to chat!</p>
+      </div>
+      </div>
       <Footer />
     </div>
   );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,11 @@
+import Spline from '@splinetool/react-spline';
 import "./home.css";
+
 
 function Home() {
   return (
     <div className="home">
-      <div className="title">
-        <h1>Hi, I'm Pranavi</h1>
-        <h2>
-          I’m a software engineer interested in digital branding and
-          AI-assisted design. <br />
-          <div className="contact">
-            <a href="mailto:pranavi_lakshminarayanan@brown.edu" target="_blank">
-              <img src="./icons/email-icon.png" alt="Email icon" />
-            </a>
-            <a href="https://github.com/prlakshm" target="_blank">
-              <img src="./icons/github-icon.png" alt="Github icon" />
-            </a>
-            <a href="https://www.linkedin.com/in/prlakshm" target="_blank">
-              <img src="./icons/linkedin-icon.png" alt="Linkedin icon" />
-            </a>
-          </div>
-        </h2>
-      </div>
-      <div className="character">
-        <img src="./home/character.png" alt="AI generated image of me" />
-      </div>
+      <Spline scene="https://prod.spline.design/ucryZISsv-cgGST1/scene.splinecode" />
     </div>
   );
 }
