0623cd0031c2ab3a2b16282669c9035eaaaca745
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-06-15 22:17:29 -0400

    Feat: contact added to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -27,6 +27,17 @@ function Home() {
           You might know me from Brown University, Tech Cadets, or Rewriting the
           Code.
         </h2>
+        <div className="contact">
+            <a href="mailto:pranavi_lakshminarayanan@brown.edu" target="_blank">
+              <img src="./icons/email-icon.png" alt="Email icon" />
+            </a>
+            <a href="https://github.com/prlakshm" target="_blank">
+              <img src="./icons/github-icon.png" alt="Github icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/prlakshm" target="_blank">
+              <img src="./icons/linkedin-icon.png" alt="Linkedin icon" />
+            </a>
+          </div>
         </div>
       </div>
       <svg>
