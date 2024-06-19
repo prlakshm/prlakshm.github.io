b57b93fef4c83638c9da4f5e3e4667203ee9231a
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-06-18 21:14:02 -0400

    Fix: removed fade in transition because sis said useless and looks like lagging

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header.js";
-import Home from "./pages/home/Home.js";
-import CaseStudy1 from "./pages/case-study-1/CaseStudy1.js";
-import CaseStudy2 from "./pages/case-study-2/CaseStudy2.js";
-import About from "./pages/about/About.js";
-import CaseStudy3 from "./pages/case-study-3/CaseStudy3.js";
-import "./app.css"
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header.js';
+import Home from './pages/home/Home.js';
+import CaseStudy1 from './pages/case-study-1/CaseStudy1.js';
+import CaseStudy2 from './pages/case-study-2/CaseStudy2.js';
+import About from './pages/about/About.js';
+import CaseStudy3 from './pages/case-study-3/CaseStudy3.js';
+import './app.css';
 
 
 function App() {
@@ -14,14 +13,14 @@ function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Home />} />
-        <Route path="/pbj-time" element={<CaseStudy1 />} />
-        <Route path="/mi-fonda" element={<CaseStudy2 />} />
-        <Route path="/artists-corner" element={<CaseStudy3 />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Home />} />
+          <Route path="/pbj-time" element={<CaseStudy1 />} />
+          <Route path="/mi-fonda" element={<CaseStudy2 />} />
+          <Route path="/artists-corner" element={<CaseStudy3 />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
     </Router>
   );
 }
