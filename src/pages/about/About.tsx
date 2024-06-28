73c09eef28f0f1700fc4640c068473b921865ed7
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-06-28 10:59:05 -0400

    Feat: twinkling star background done

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,41 +3,116 @@ import Footer from "../../components/Footer.js";
 import "./about.css";
 
 function About() {
-
   const waterElementRef = useRef<HTMLDivElement>(null);
 
-    // This will run once when the component mounts scroll to top page
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []); 
+  const sizes = [
+    [2, 2],
+    [5, 5],
+    [7, 7],
+    [4, 4],
+    [8, 8]
+  ];
 
-    useEffect(() => {
-      // Reapply the SVG filter when the component mounts
-      if (waterElementRef.current) {
-        waterElementRef.current.style.filter = "url(#turbulence)";
-      }
-      if (waterElementRef.current) {
-        waterElementRef.current.style.filter = "url(#turbulence2)";
-  
-      }
-    }, []);
-    
+  //get random position between 1 - 100;
+  function randomPosition(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const size = sizes[Math.floor(Math.random() * sizes.length)]; // Select random size from sizes array
+
+  // This will run once when the component mounts scroll to top page
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    // Reapply the SVG filter when the component mounts
+    if (waterElementRef.current) {
+      waterElementRef.current.style.filter = "url(#turbulence)";
+    }
+    if (waterElementRef.current) {
+      waterElementRef.current.style.filter = "url(#turbulence2)";
+    }
+  }, []);
+
   return (
     <div className="about">
-      <div className="about-page"><div className="water" ref={waterElementRef}></div>
-      <div className="main">
-      <div className="about-me">
-        <h1>About Me</h1>
-        <p>I'm Pranavi 👋, a <span className="bold">sophomore at Brown University</span> studying computer science. I name all my ML models and paste pictures of websites I've built in my room! 
-            When I'm not coding, I'm singing, dancing, and having a good time with my friends.</p>
-            <p>I'm part of <span className="bold">Full Stack @ Brown</span>, where I work in a team of designers and developers to code websites for student clubs. I'm also a member of <span className="bold">Rewriting the Code</span>, a global organization that helps empower and connect women in CS. 
-                Previously, I was an encryption intern at <a href="https://www.mantech.com/" target="_blank">ManTech</a>, where I learned I love algorithms, and currently am a data science intern at <a href="https://www.carync.gov/" target="_blank">Town of Cary</a>, a smart city and the <a href="https://livability.com/best-places/2024-top-100-best-places-to-live-in-the-us/" target="_blank">#2 best city to live in the US</a>!
+      <div className="about-page">
+      {[...Array(110)].map((_, index) => {
+         // Randomly select size from sizes array
+         const sizeIndex = Math.floor(Math.random() * sizes.length);
+         let [height, width] = sizes[sizeIndex];
+          // Determine if it's a jumbo star
+          if (index < 10) {
+            [height, width] = [17, 15]; // Jumbo size
+          } 
+        let starClass = "star1"; // Default to star1, adjust as needed
+        if (index > 20 && index <= 40) starClass = "star2";
+        else if (index > 40 && index <= 60) starClass = "star3";
+        else if (index > 60 && index <= 80) starClass = "star4";
+        else if (index > 80 && index <= 100) starClass = "star5";
+
+        return (
+          <div
+            key={index}
+            className={`star ${starClass}`}
+            style={{
+              top: `${randomPosition(1, 82)}%`,
+              left: `${randomPosition(1, 98)}%`,
+              height: `${height}px`,
+              width: `${width}px`, // Ensure both height and width are the same
+            }}
+          ></div>
+        );
+      })}
+        <div className="water" ref={waterElementRef}></div>
+        <div className="main">
+          <div className="about-me">
+            <h1>About Me</h1>
+            <p>
+              I'm Pranavi 👋, a{" "}
+              <span className="bold">sophomore at Brown University</span>{" "}
+              studying computer science. I name all my ML models and paste
+              pictures of websites I've built in my room! When I'm not coding,
+              I'm singing, dancing, and having a good time with my friends.
             </p>
-            <p>Outside of CS, I take literary arts classes at Brown where I explore different forms of storytelling. <span className="bold">I love movies</span> (ask me for recs!) and recently got into screenwriting!  </p>
-            <p>If you're also interested in any of these things, I'd love to chat!</p>
-      </div>
-      </div>
-      <Footer />
+            <p>
+              I'm part of <span className="bold">Full Stack @ Brown</span>,
+              where I work in a team of designers and developers to code
+              websites for student clubs. I'm also a member of{" "}
+              <span className="bold">Rewriting the Code</span>, a global
+              organization that helps empower and connect women in CS.
+              Previously, I was an encryption intern at{" "}
+              <a href="https://www.mantech.com/" target="_blank">
+                ManTech
+              </a>
+              , where I learned I love algorithms, and currently am a data
+              science intern at{" "}
+              <a href="https://www.carync.gov/" target="_blank">
+                Town of Cary
+              </a>
+              , a smart city and the{" "}
+              <a
+                href="https://livability.com/best-places/2024-top-100-best-places-to-live-in-the-us/"
+                target="_blank"
+              >
+                #2 best city to live in the US
+              </a>
+              !
+            </p>
+            <p>
+              Outside of CS, I take literary arts classes at Brown where I
+              explore different forms of storytelling.{" "}
+              <span className="bold">I love movies</span> (ask me for recs!) and
+              recently got into screenwriting!{" "}
+            </p>
+            <p>
+              If you're also interested in any of these things, I'd love to
+              chat!
+            </p>
+          </div>
+        </div>
+        <Footer />
       </div>
       <svg>
         <filter id="turbulence" x="0" y="0" width="100%" height="100%">
