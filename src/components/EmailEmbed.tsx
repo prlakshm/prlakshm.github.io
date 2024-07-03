f655f52591514fef087ce56d728d3ab334218ca5
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-07-03 15:37:53 -0400

    Feat: contact me formatted

--- src/components/EmailEmbed.tsx
+++ src/components/EmailEmbed.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Cal, { getCalApi } from "@calcom/embed-react";
 import "./email-embed.css";
 
-function EmailEmbed() { 
+function EmailEmbed() {
   const [formState, setFormState] = useState({
     name: "",
     email: "",
@@ -65,13 +65,19 @@ function EmailEmbed() {
 
   useEffect(() => {
     (async function () {
-      const cal = await getCalApi();
-      cal("ui", {
-        theme: "dark",
-        styles: { branding: { brandColor: "#f8dcff" } },
-        hideEventTypeDetails: false,
-        layout: "month_view",
-        cssVarsPerTheme: {
+      try {
+        const cal = await getCalApi();
+        // Preload the Cal component
+        cal("floatingButton", {
+            calLink: "pranavil/30min",
+          });
+        // Customize UI elements
+        cal("ui", {
+          theme: "dark",
+          styles: { branding: { brandColor: "#f8dcff" } },
+          hideEventTypeDetails: false,
+          layout: "month_view",
+          cssVarsPerTheme: {
             light: {},
             dark: {
               "cal-brand": "#f8dcff",
@@ -89,124 +95,135 @@ function EmailEmbed() {
               "cal-border-booker": "#f8dcff",
               "cal-text-muted": "#ccb6d2",
               "cal-border-muted": "#f8dcff",
-              "cal-bg-emphasis": "#575757"
-            }
-        }
-      });
+              "cal-bg-emphasis": "#575757",
+            },
+          },
+        });
+      } catch (error) {
+        console.error("Error loading Cal:", error);
+      }
     })();
   }, []);
 
   return (
     <div className="email-embed">
-        <ul className="tab-list">
-        <li className={activeTab === "email" ? "active" : ""}>
+      <div className="tab-content" id="myTabContent">
+      <ul className="tab-list">
+        <li>
           <button onClick={() => setActiveTab("email")}>Message Me</button>
         </li>
-        <li className={activeTab === "schedule" ? "active" : ""}>
-          <button onClick={() => setActiveTab("schedule")}>Schedule a Meeting</button>
+        <li>
+          <button onClick={() => setActiveTab("schedule")}>
+            Schedule a Meeting
+          </button>
         </li>
       </ul>
-    <div className="tab-content" id="myTabContent">
-      <div
-        aria-labelledby="form-tab"
-        className={`tab-pane flex-center ${activeTab === "email" ? "active" : ""}`}
-        id="form"
-        role="tabpanel"
-      >
-        <form
-          onSubmit={handleSubmit}
-          className="dark-mode-form flex-center"
-        >
-          <div className="form-group" style={{marginTop:"0.25rem"}}>
-            <label htmlFor="name">Name:</label>
-            <input
-              className="form-control"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-              required
-              type="text"
-              value={formState.name}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              className="form-control"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-              required
-              type="email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="reason">Reason:</label>
-            <div className="custom-dropdown">
-              <select
-                className="form-control"
-                id="reason"
-                name="reason"
-                value={formState.reason}
-                onChange={handleChange}
+        {activeTab === "email" ? (
+          <div
+            aria-labelledby="form-tab"
+            className="tab-pane flex-center"
+            id="form"
+            role="tabpanel"
+          >
+            <form
+              onSubmit={handleSubmit}
+              className="dark-mode-form flex-center"
+            >
+              <div className="form-group" style={{ marginTop: "0.25rem" }}>
+                <label htmlFor="name">Name:</label>
+                <input
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  placeholder="Enter your name"
+                  required
+                  type="text"
+                  value={formState.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  required
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="reason">Reason:</label>
+                <div className="custom-dropdown">
+                  <select
+                    className="form-control"
+                    id="reason"
+                    name="reason"
+                    value={formState.reason}
+                    onChange={handleChange}
+                  >
+                    <option disabled selected value="">
+                      Select a reason
+                    </option>
+                    <option value="Brown University">Brown University</option>
+                    <option value="Networking">Career + Networking</option>
+                    <option value="Coding Projects">
+                      Questions on Coding Projects
+                    </option>
+                    <option value="Other">Other</option>
+                  </select>
+                  <span className="arrow"></span>
+                </div>
+              </div>
+              <div className="extra-field">
+                <input name="_gotcha" type="text" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message:</label>
+                <textarea
+                  className="form-control"
+                  id="message"
+                  name="message"
+                  placeholder="Enter your message"
+                  required
+                  rows={5}
+                  value={formState.message}
+                  onChange={handleChange}
+                />
+              </div>
+              <button
+                className="submit-button"
+                type="submit"
+                disabled={isSubmitting}
               >
-                <option disabled selected value="">
-                  Select a reason
-                </option>
-                <option value="Brown University">Brown University</option>
-                <option value="Networking">Career + Networking</option>
-                <option value="Coding Projects">
-                  Questions on Coding Projects
-                </option>
-                <option value="Other">Other</option>
-              </select>
-              <span className="arrow"></span>
+                {isSubmitting ? "Sending..." : "Send"}
+              </button>
+            </form>
+            {responseMessage && (
+              <div className="response-message" id="responseContainer">
+                <p>{responseMessage}</p>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div
+            aria-labelledby="schedule-tab"
+            className="tab-pane"
+            id="scheduleMeet"
+            role="tabpanel"
+          >
+            <div id="my-cal-inline">
+              <Cal
+                calLink="pranavil/30min"
+              ></Cal>
             </div>
           </div>
-          <div className="extra-field">
-            <input name="_gotcha" type="text" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              className="form-control"
-              id="message"
-              name="message"
-              placeholder="Enter your message"
-              required
-              rows={5}
-              value={formState.message}
-              onChange={handleChange}
-            />
-          </div>
-          <button
-            className="submit-button"
-            type="submit"
-            disabled={isSubmitting}
-          >
-            {isSubmitting ? "Sending..." : "Send"}
-          </button>
-        </form>
-        {responseMessage && (
-          <div className="response-message" id="responseContainer">
-            <p>{responseMessage}</p>
-          </div>
         )}
+        ;
       </div>
-      <div
-        aria-labelledby="schedule-tab"
-        className={`tab-pane ${activeTab === "schedule" ? "active" : ""}`}
-        id="scheduleMeet"
-        role="tabpanel"
-      >
-        <div id="my-cal-inline">
-          <Cal calLink="pranavil/30min" config={{ layout: "month_view" }}></Cal>
-        </div>
-      </div>
-    </div>
     </div>
   );
 }
